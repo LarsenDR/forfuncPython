dad94a9c32274194f2ee50d6de61ccd2648388f7
Dave Larsen <larsendr@missouri.edu>
2019-01-21 10:07:51 -0600

    fixed And typos

--- python/acornwt.py
+++ python/acornwt.py
@@ -10,13 +10,13 @@ def acornwt(dbh, species)
   elif((species == "chestnut") && (dbh > 9.9) && (dbh < 36.1)):
     acornwt = 0.0008271 * dbh ^ 3 - 0.08157 * dbh ^ 2 + 2.692 * dbh - 18.85
 
-  elif(species = "red" And dbh > 9.9 And dbh < 36.1):
+  elif(species = "red" && dbh > 9.9 && dbh < 36.1):
     acornwt = 0.0004016 * dbh ^ 4 - 0.0349937 * dbh ^ 3 + 0.9864357 * dbh ^ 2 - 9.5233885 * dbh + 27.32720516
 
-  elif(species = "scarlet" And dbh > 9.9 And dbh < 36.1):
+  elif(species = "scarlet" && dbh > 9.9 && dbh < 36.1):
     acornwt = 0.0005975 * dbh ^ 4 - 0.05325 * dbh ^ 3 + 1.651 * dbh ^ 2 - 19.97 * dbh + 84.71
 
-  elif(species = "white" And dbh > 9.9 And dbh < 36.1):
+  elif(species = "white" && dbh > 9.9 && dbh < 36.1):
     acornwt = 0.0001987 * dbh ^ 4 - 0.02027 * dbh ^ 3 + 0.694 * dbh ^ 2 - 8.768 * dbh + 37.36
 
   else
